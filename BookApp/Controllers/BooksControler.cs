328427a2120235fe751e34aa14d1ff4ff6c8575b
Miguel-124 <michalgorecki124@gmail.com>
2023-10-14 09:26:32 +0200

    start

---
+++ BookApp/Controllers/BooksControler.cs
@@ -0,0 +1,91 @@
+﻿using BookApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Controllers
+{
+    public class BooksControler : Controller
+    {
+        // GET: BooksControler
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BooksControler/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BooksControler/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BooksControler/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksControler/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksControler/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Random
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Author = "Random Author", Title = "Random Title" };
+            return View(firstBook);
+        }
+
+        // GET: BooksControler/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksControler/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BookApp/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace BookApp.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
